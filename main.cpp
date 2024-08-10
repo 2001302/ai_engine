e2b93dd9e1f0d8ca61c8474102f921c8152cbf32
kyo <>
2024-08-11 03:02:11 +0900

    clean up

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-#include "llama_handler.h"
+#include "llama_model.h"
+#include "stable_diffusion_model.h"
 
 int main(int argc, char** argv) {
 
-    LlamaHandler llama;
-    llama.Run(argc, argv);
+    int mode = 0;
+    LlamaModel llama;
+    StableDiffusionModel stable_diffusion;
 
+    while (true) {
+        switch (mode)
+        {
+        case 0: {
+            llama.Run(argc, argv);
+            break;
+        }
+        case 1: {
+            stable_diffusion.Run(argc, argv);
+            break;
+        }
+        default:
+            break;
+        }
+        
+    }
+    
     return 0;
 }
