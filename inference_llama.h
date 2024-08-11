b9ca9e5e097898a175e6027134dee36ed11d2cbc
kyo <yoonjunkyo@gmail.com>
2024-08-11 14:20:56 +0900

    clean up

--- inference_llama.h
+++ inference_llama.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <filesystem>
 #include "llama.cpp\common\common.h"
+#include "inference.h"
 
 static void print_usage(int argc, char** argv, const gpt_params& params) {
 	gpt_params_print_usage(argc, argv, params);
@@ -11,7 +12,7 @@ static void print_usage(int argc, char** argv, const gpt_params& params) {
 	LOG_TEE("\n");
 }
 
-class LlamaModel
+class InferenceLlama : public Inference<gpt_params>
 {
 public:
 	int Run(int argc, char** argv) {
@@ -22,8 +23,8 @@ public:
 		params.n_predict = 32;
 
 		if (!gpt_params_parse(argc, argv, params)) {
-		    print_usage(argc, argv, params);
-		    return 1;
+			print_usage(argc, argv, params);
+			return 1;
 		}
 		params.model = "model/llama-2-7b-chat.Q2_K.gguf";
 
@@ -39,8 +40,8 @@ public:
 		llama_model* model = llama_load_model_from_file(params.model.c_str(), model_params);
 
 		if (model == NULL) {
-		    fprintf(stderr, "%s: error: unable to load model\n", __func__);
-		    return 1;
+			fprintf(stderr, "%s: error: unable to load model\n", __func__);
+			return 1;
 		}
 
 		// initialize the context
@@ -48,8 +49,8 @@ public:
 		llama_context* ctx = llama_new_context_with_model(model, ctx_params);
 
 		if (ctx == NULL) {
-		    fprintf(stderr, "%s: error: failed to create the llama_context\n", __func__);
-		    return 1;
+			fprintf(stderr, "%s: error: failed to create the llama_context\n", __func__);
+			return 1;
 		}
 
 		// tokenize the prompt
@@ -63,16 +64,16 @@ public:
 
 		// make sure the KV cache is big enough to hold all the prompt and generated tokens
 		if (n_kv_req > n_ctx) {
-		    LOG_TEE("%s: error: n_kv_req > n_ctx, the required KV cache size is not big enough\n", __func__);
-		    LOG_TEE("%s:        either reduce n_predict or increase n_ctx\n", __func__);
-		    return 1;
+			LOG_TEE("%s: error: n_kv_req > n_ctx, the required KV cache size is not big enough\n", __func__);
+			LOG_TEE("%s:        either reduce n_predict or increase n_ctx\n", __func__);
+			return 1;
 		}
 
 		// print the prompt token-by-token
 		fprintf(stderr, "\n");
 
 		for (auto id : tokens_list) {
-		    fprintf(stderr, "%s", llama_token_to_piece(ctx, id).c_str());
+			fprintf(stderr, "%s", llama_token_to_piece(ctx, id).c_str());
 		}
 
 		fflush(stderr);
@@ -83,15 +84,15 @@ public:
 
 		// evaluate the initial prompt
 		for (size_t i = 0; i < tokens_list.size(); i++) {
-		    llama_batch_add(batch, tokens_list[i], i, { 0 }, false);
+			llama_batch_add(batch, tokens_list[i], i, { 0 }, false);
 		}
 
 		// llama_decode will output logits only for the last token of the prompt
 		batch.logits[batch.n_tokens - 1] = true;
 
 		if (llama_decode(ctx, batch) != 0) {
-		    LOG_TEE("%s: llama_decode() failed\n", __func__);
-		    return 1;
+			LOG_TEE("%s: llama_decode() failed\n", __func__);
+			return 1;
 		}
 
 		// main loop
@@ -101,49 +102,49 @@ public:
 		const auto t_main_start = ggml_time_us();
 
 		while (n_cur <= n_predict) {
-		    // sample the next token
-		    {
-		        auto   n_vocab = llama_n_vocab(model);
-		        auto* logits = llama_get_logits_ith(ctx, batch.n_tokens - 1);
+			// sample the next token
+			{
+				auto   n_vocab = llama_n_vocab(model);
+				auto* logits = llama_get_logits_ith(ctx, batch.n_tokens - 1);
 
-		        std::vector<llama_token_data> candidates;
-		        candidates.reserve(n_vocab);
+				std::vector<llama_token_data> candidates;
+				candidates.reserve(n_vocab);
 
-		        for (llama_token token_id = 0; token_id < n_vocab; token_id++) {
-		            candidates.emplace_back(llama_token_data{ token_id, logits[token_id], 0.0f });
-		        }
+				for (llama_token token_id = 0; token_id < n_vocab; token_id++) {
+					candidates.emplace_back(llama_token_data{ token_id, logits[token_id], 0.0f });
+				}
 
-		        llama_token_data_array candidates_p = { candidates.data(), candidates.size(), false };
+				llama_token_data_array candidates_p = { candidates.data(), candidates.size(), false };
 
-		        // sample the most likely token
-		        const llama_token new_token_id = llama_sample_token_greedy(ctx, &candidates_p);
+				// sample the most likely token
+				const llama_token new_token_id = llama_sample_token_greedy(ctx, &candidates_p);
 
-		        // is it an end of generation?
-		        if (llama_token_is_eog(model, new_token_id) || n_cur == n_predict) {
-		            LOG_TEE("\n");
+				// is it an end of generation?
+				if (llama_token_is_eog(model, new_token_id) || n_cur == n_predict) {
+					LOG_TEE("\n");
 
-		            break;
-		        }
+					break;
+				}
 
-		        LOG_TEE("%s", llama_token_to_piece(ctx, new_token_id).c_str());
-		        fflush(stdout);
+				LOG_TEE("%s", llama_token_to_piece(ctx, new_token_id).c_str());
+				fflush(stdout);
 
-		        // prepare the next batch
-		        llama_batch_clear(batch);
+				// prepare the next batch
+				llama_batch_clear(batch);
 
-		        // push this new token for next evaluation
-		        llama_batch_add(batch, new_token_id, n_cur, { 0 }, true);
+				// push this new token for next evaluation
+				llama_batch_add(batch, new_token_id, n_cur, { 0 }, true);
 
-		        n_decode += 1;
-		    }
+				n_decode += 1;
+			}
 
-		    n_cur += 1;
+			n_cur += 1;
 
-		    // evaluate the current batch with the transformer model
-		    if (llama_decode(ctx, batch)) {
-		        fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
-		        return 1;
-		    }
+			// evaluate the current batch with the transformer model
+			if (llama_decode(ctx, batch)) {
+				fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
+				return 1;
+			}
 		}
 
 		LOG_TEE("\n");
@@ -151,7 +152,7 @@ public:
 		const auto t_main_end = ggml_time_us();
 
 		LOG_TEE("%s: decoded %d tokens in %.2f s, speed: %.2f t/s\n",
-		    __func__, n_decode, (t_main_end - t_main_start) / 1000000.0f, n_decode / ((t_main_end - t_main_start) / 1000000.0f));
+			__func__, n_decode, (t_main_end - t_main_start) / 1000000.0f, n_decode / ((t_main_end - t_main_start) / 1000000.0f));
 
 		llama_print_timings(ctx);
 
